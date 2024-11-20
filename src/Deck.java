cc5b09943dcd63b27a561e38d36e7d17f159e049
jaredsaal <jared.saal@menloschool.org>
2024-11-19 21:40:07 -0800

    Finished Deck class (untested)

--- src/Deck.java
+++ src/Deck.java
@@ -5,9 +5,9 @@ public class Deck {
     private int cardsLeft;
 
     public Deck(String[] ranks, String[] suits, int[] values) {
-        this.cardsLeft = 52;
         for (int i = 0; i < ranks.length; i++) {
             deck.add(new Card(ranks[i], suits[i], values[i]));
+            cardsLeft++;
         }
     }
 
@@ -21,6 +21,16 @@ public class Deck {
     public Card deal() {
         if (isEmpty()) {
             return null;
+        }
+        return deck.get(--cardsLeft);
+    }
+
+    public void shuffle() {
+        for (int i = cardsLeft - 1; i > 0; i--) {
+            Card placeholder = deck.remove(i);
+            int random = (int) (i * Math.random());
+            deck.add(i, deck.get(random));
+            deck.add(random, placeholder);
         }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -7,7 +7,6 @@ public class Player {
 
     public Player(String name) {
         this.points = 0;
-        // QUESTION
         this.hand = new ArrayList<Card>();
     }
 
